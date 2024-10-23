b49b1fe846da6ad4279279ec83b7c3adf4c29ee0
Nash <nash@chaudhary.local>
2024-10-24 02:21:04 +0900

    Update configuration Add jest configuration Modify typescript configuration Modify babel configuration

---
+++ population-trends-spa/jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["js", "ts", "vue"],
+    transform: {
+      "^.+\\.vue$": "@vue/vue3-jest",
+      "^.+\\.(ts|tsx)$": "ts-jest",
+      "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    testMatch: ["**/tests/**/*.spec.ts", "**/__tests__/*.(ts|js)"],
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    transformIgnorePatterns: [
+      "/node_modules/(?!axios)",
+    ],
+    testEnvironmentOptions: {
+      customExportConditions: ["node", "node-addons", "worker"],
+    },
+    globals: {
+      "ts-jest": {
+        tsconfig: "tsconfig.json",
+      },
+    },
+  };
+  
