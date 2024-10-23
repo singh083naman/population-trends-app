4f2ffc90b7f754bd984460c407a40a7ba3912ce2
Nash <nash@chaudhary.local>
2024-10-24 02:13:16 +0900

    Update main typescript file Add global scope for the necessary module

--- population-trends-spa/src/main.ts
+++ population-trends-spa/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import HighchartsVue from "highcharts-vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(HighchartsVue);
+app.mount("#app");
